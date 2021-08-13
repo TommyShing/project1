627ec668248a8c1af6743d8b93a23b913843264c
TommyShing <62638488+TommyShing@users.noreply.github.com>
2021-08-13 17:42:58 +0800

    Update gittest_beta.py

--- gittest_beta.py
+++ gittest_beta.py
@@ -24,11 +24,12 @@ acc_input.send_keys(username)
 pwd_input = browser.find_element_by_xpath('//*[@id="password"]')
 pwd_input.send_keys(password)
 
+browser.find_element_by_xpath('//*[@id="remember"]').click()
+
 browser.find_element_by_xpath('//*[@id="page-content-wrapper"]/div[2]/div/div/div/div/form/button').click()
 time.sleep(1)
 
-browser.find_element_by_xpath('//*[@id="page-content-wrapper"]/div[2]/div/div/div/table/tbody/tr/td[1]/a/div/div[1]').click()
+browser.get('https://www.xshellz.com/xpanel/shell/135211')
 time.sleep(1)
-
 browser.find_element_by_xpath('//*[@id="page-content-wrapper"]/div[11]/div/div/table/tbody/tr[7]/td[2]/div/div[1]/button').click()
 time.sleep(1)
